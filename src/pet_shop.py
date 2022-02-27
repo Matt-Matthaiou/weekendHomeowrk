572cc3e0acd629a3c759c350998ecc81433ddc8d
Matt-Matthaiou <mat.matthaiou@gmail.com>
2022-02-27 15:04:17 +0000

    weekeend homework

---
+++ src/pet_shop.py
@@ -0,0 +1,82 @@
+# WRITE YOUR FUNCTIONS HERE
+ 
+def get_pet_shop_name(pet_shop):
+     for items in pet_shop:
+        return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, amount):
+    pet_shop["admin"]['total_cash'] += amount
+    return pet_shop["admin"]["total_cash"]
+    
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, number):
+    pet_shop["admin"]["pets_sold"] += number
+    return pet_shop["admin"]["pets_sold"]
+
+def get_stock_count(pet_shop,):
+    count = 0
+    for pet in pet_shop["pets"]:
+        count += 1
+    return count
+
+def get_pets_by_breed(pet_shop, breed):
+    pets_of_breed = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets_of_breed.append(pet)
+    return pets_of_breed
+
+def find_pet_by_name(pet_shop, name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == name:
+            return pet
+
+def remove_pet_by_name(pet_shop, name):
+    for pet in pet_shop['pets']:
+        if pet["name"] == name:
+            pet_shop['pets'].remove(pet)
+
+def add_pet_to_stock(pet_shop, new_pet):
+    pet_shop["pets"].append(new_pet)
+
+def get_customer_cash(customer):
+    return customer['cash']
+
+def remove_customer_cash(customer, amount):
+    customer['cash'] -= amount
+
+def get_customer_pet_count(customer):
+    count = 0
+    for pet in customer['pets']:
+        count += 1
+    return count
+
+def add_pet_to_customer(customer, new_pet):
+    customer["pets"].append(new_pet)
+
+def customer_can_afford_pet(customer, pet):
+    if customer["cash"] >= pet["price"]:
+        return True
+    else: 
+        return False
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if pet != None:
+            if customer_can_afford_pet(customer, pet) == True:
+                remove_pet_by_name(pet_shop, pet['name'])
+                add_or_remove_cash(pet_shop, pet["price"])
+                remove_customer_cash(customer, pet["price"])
+                add_pet_to_customer(customer, pet)
+                increase_pets_sold(pet_shop, 1) 
+     
+            
+    
+
+          
+            
+    
